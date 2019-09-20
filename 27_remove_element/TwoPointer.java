aa869bb88386063492d16f57336753445708b0e7
Xinlin Song <fluffy400@gmail.com>
2019-09-19 22:28:32 -0500

    Finished Q27 remove element.

--- 27_remove_element/TwoPointer.java
+++ 27_remove_element/TwoPointer.java
@@ -19,7 +19,7 @@ public class TwoPointer {
         else {
             int p1 = 0;
             int p2 = findNextP2(nums, val, nums.length-1);
-            while (p1 < p2) {
+            while (p1 <= p2) {
                 if (nums[p1] == val) {
                     swap(nums, p1, p2);
                     p2 = findNextP2(nums, val, p2);
